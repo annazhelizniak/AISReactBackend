3a158a281ad8af114448c422a6c88203cc2fada8
Zhelizniak Anna <anna.zhelizniak@ukma.edu.ua>
2023-04-29 11:31:59 +0300

    categories

--- dao/dbProperties.js
+++ dao/dbProperties.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 exports.connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '12345',
     database: 'zlagoda'
 });
 
